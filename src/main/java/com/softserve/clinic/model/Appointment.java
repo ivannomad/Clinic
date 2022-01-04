50bc184d52ccca5c5c0d566f14dfbaacfd405e8e
Roman <romagrabovetskiy@gmail.com>
2022-01-04 10:40:52 +0200

    Appointment updated, added references for doctor and patient.

--- src/main/java/com/softserve/clinic/model/Appointment.java
+++ src/main/java/com/softserve/clinic/model/Appointment.java
@@ -5,22 +5,30 @@ import java.util.Date;
 public class Appointment {
 
     private int id;
-    Date date=new Date();
+    Date date;
     private int doctor_id;
     private int patient_id;
+    Doctor doctor;
+    Patient patient;
 
 
-    public Appointment(int id, Date date, int doctor_id, int patient_id) {
+    public Appointment(int id,Doctor doctor,Patient patient) {
         this.id = id;
-        this.date = date;
-        this.doctor_id = doctor_id;
-        this.patient_id = patient_id;
+        this.date = new Date();
+        this.doctor=doctor;
+        this.patient=patient;
+        this.doctor_id = doctor.getId();
+        this.patient_id = patient.getId();
+
+
     }
     public Appointment() {
         this.id = id;
-        this.date = date;
-        this.doctor_id = doctor_id;
-        this.patient_id = patient_id;
+        this.date = new Date();
+        this.doctor=doctor;
+        this.patient=patient;
+        this.doctor_id = doctor.getId();
+        this.patient_id = patient.getId();
     }
 
     public int getId() {
@@ -53,5 +61,21 @@ public class Appointment {
 
     public void setPatient_id(int patient_id) {
         this.patient_id = patient_id;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
     }
 }

c3d8f594a60db4acff3a07c77b16255d31f332dd
Roman <romagrabovetskiy@gmail.com>
2022-01-04 10:51:18 +0200

    doctor, corrected

--- src/main/java/com/softserve/clinic/model/Doctor.java
+++ src/main/java/com/softserve/clinic/model/Doctor.java
@@ -3,14 +3,14 @@ package com.softserve.clinic.model;
 public class Doctor extends User {
     private String specialization;
 
-    public Doctor(int id, String username, String password, String first_name, String second_name, String email, String contact_number, String specialization) {
+    public Doctor(int id, String username, String password, String first_name, String second_name, String email, String contact_number, Specialization specialization) {
         super(id, username, password, first_name, second_name, email, contact_number);
-        this.specialization = specialization;
+        this.specialization = specialization.getSpecialization();
     }
 
-    public Doctor() {
+    public Doctor(Specialization specialization) {
         super();
-        this.specialization = specialization;
+        this.specialization = specialization.getSpecialization();
     }
 
     public String getSpecialization() {

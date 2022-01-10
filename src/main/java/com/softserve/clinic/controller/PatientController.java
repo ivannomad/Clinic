2dbc9ebf38220a32dde394b106febcb3712c4faf
ivannomad <54027265+ivannomad@users.noreply.github.com>
2022-01-10 12:59:07 +0200

    Added some files (#10)
    Co-authored-by: Zraichenko Oleh <0789oleh>

--- src/main/java/com/softserve/clinic/controller/PatientController.java
+++ src/main/java/com/softserve/clinic/controller/PatientController.java
@@ -1,4 +1,43 @@
 package com.softserve.clinic.controller;
 
+
+import com.softserve.clinic.dto.PatientDto;
+import com.softserve.clinic.dto.PatientDto;
+import com.softserve.clinic.model.Patient;
+import com.softserve.clinic.service.impl.PatientServiceImpl;
+import com.softserve.clinic.service.impl.PatientServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/patients")
 public class PatientController {
+    private final PatientServiceImpl patientService;
+
+    @Autowired
+    public PatientController(PatientServiceImpl PatientService) {
+        this.patientService = PatientService;
+    }
+
+    @PostMapping
+    public PatientDto createPatient(PatientDto patientDto) {
+        return patientService.createPatient(patientDto);
+    }
+
+    @GetMapping
+    public List<Patient> getAllPatients() {
+        return patientService.getAllPatients();
+    }
+
+    @GetMapping("/{id}")
+    public Patient getPatientById(UUID id) {
+        return patientService.getPatientById(id);
+    }
+
 }
---
+++ src/test/java/com/softserve/clinic/repository/PatientRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.softserve.clinic.repository;
+
+
+import com.softserve.clinic.model.Patient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PatientRepositoryTest {
+    private final PatientRepository patientRepository;
+
+   PatientRepositoryTest(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    @Test
+    void save() {
+       Patient patient = new Patient();
+       patient.setFirstName("Klavdija");
+       patientRepository.save(patient);
+       assertTrue(patient.getId()!=null);
+    }
+
+    @Sql(statements = "insert into users ('username','password', 'first_name'," +
+            " 'second_name', 'email', 'contact_number', 'role')" +
+            "values ('bg_zn', '123456', 'Bogdan', 'Zinovii', 'bg_zn@mail.com', '223145666', 'PATIENT ')")
+    @Test
+    void update() {
+       Patient actual =patientRepository.findByUserName("bg_zn");
+        assertNotNull(actual);
+        actual.setFirstName("Katerina");
+       Patient expected =patientRepository.save(actual);
+        assertNotNull(expected);
+        assertEquals(expected, actual);
+    }
+}

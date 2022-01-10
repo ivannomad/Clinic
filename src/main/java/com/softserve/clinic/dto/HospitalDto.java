4de096d1167d92f0c4e622b232981c70f91241f1
SchoolboyIQ <74767650+SchoolboyIQ@users.noreply.github.com>
2022-01-10 19:06:48 +0200

    Hospital impl

---
+++ src/main/java/com/softserve/clinic/dto/HospitalDto.java
@@ -0,0 +1,26 @@
+package com.softserve.clinic.dto;
+
+import lombok.Data;
+import lombok.Value;
+
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Value
+public class HospitalDto implements Serializable {
+
+    UUID id;
+
+    @NotBlank(message = "Hospital name must not be blank")
+    String hospitalName;
+
+    @NotBlank(message = "City may must be blank")
+    String city;
+
+    @NotBlank(message = "Street name must not be blank")
+    String street;
+
+    @NotBlank(message = "Address number must not be blank")
+    String addressNumber;
+}
